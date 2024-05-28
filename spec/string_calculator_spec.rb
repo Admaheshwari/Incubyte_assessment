f831e9505d68543f890bfef3d32d3fe9ac8db7b9
Akash deep <akash@ktechcomputing.in>
2024-05-28 21:59:07 +0530

    Return the sum of numbers given with \n

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,19 +12,25 @@ RSpec.describe StringCalculator do
 
     context 'when input is a single number' do
       it 'returns the input number' do
-        expect(StringCalculator.add('5')).to eq(5)
+        expect(StringCalculator.add('1')).to eq(1)
       end
     end
 
     context 'when input is two numbers' do 
     	it 'return the sum of the two numbers' do 
-    		expect(StringCalculator.add('2,7')).to eq(9)
+    		expect(StringCalculator.add('1,5')).to eq(6)
     	end
     end
 
     context 'when input is multiple numbers' do 
     	it 'return the sum of multiple number string' do 
     		expect(StringCalculator.add('4,2,7,9,10,15,35')).to eq(82)
+    	end
+    end
+
+    context 'when input contains \n in between numbers' do 
+    	it 'return the sum of the numbers given' do 
+    		expect(StringCalculator.add('1\n2,3')).to eq(6)
     	end
     end
 
