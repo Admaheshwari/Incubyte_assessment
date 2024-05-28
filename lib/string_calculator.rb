b6c526d50f343481da285417e36924c0d20a8b56
Akash deep <akash@ktechcomputing.in>
2024-05-28 21:49:15 +0530

    Return the sum of two numbers if the params contain 2 numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,5 +3,8 @@ class StringCalculator
     return 0 if numbers.empty?  #I assume if we getting number as empty then return 0
 
     return numbers.to_i if numbers.length == 1 # return the same no if we have the length eq to 1
+
+    int_numbers = numbers.split(',').map(&:to_i) # split the number from comma and make them array of integer
+    int_numbers.sum
   end
 end
