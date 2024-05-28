f7e9083eeff6623b52e9ef3d75ee6ca74b849068
Akash deep <akash@ktechcomputing.in>
2024-05-28 21:37:48 +0530

    Return the same no if we have single number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,7 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?  #I assume if we getting number as empty then return 0
+
+    return numbers.to_i if numbers.length == 1 # return the same no if we have the length eq to 1
   end
 end
