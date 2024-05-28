7358032e1b16fecaff65c7446efc0600bbbf6371
Akash deep <akash@ktechcomputing.in>
2024-05-28 22:28:35 +0530

    Return the result as expected if we pass the different delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,7 +4,16 @@ class StringCalculator
 
     return numbers.to_i if numbers.length == 1 # return the same no if we have the length eq to 1
 
-    int_numbers = numbers.split(/,|\n/).map(&:to_i) # split the number from comma and make them array of integer
+    delimiter = /,|\n/ 
+
+    if numbers.start_with?("//") # check whether no started with // or not
+      separate_number = numbers.split("\n", 2)   # separate the number and unwanted characters
+      unwanted_char = separate_number[0][2..-1]
+      numbers = separate_number[1]  # assign the first index to numbers
+      delimiter = Regexp.escape(unwanted_char) # to escape any special characters
+    end
+
+    int_numbers = numbers.split(Regexp.union(delimiter)).map(&:to_i) # split the number from comma and make them array of integer
     int_numbers.sum
   end
 end
